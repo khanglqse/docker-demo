26ba777d7803040a7a62def168313f2f0dcbc663
Khang Luong <khanglqse@gmail.com>
2021-05-04 16:53:16 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  data: any
+  constructor(private http: HttpClient) {
+
+  }
   title = 'frontend';
+  ngOnInit() {
+    this.http.get('http://localhost:3000')
+    .subscribe(data => this.data = data) 
+  }
+  
 }
